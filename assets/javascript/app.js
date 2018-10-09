1093e89589d2c3484b798b239a00d9179bf092e4
Will H <wandrewhouston@gmail.com>
2018-10-09 19:55:03 -0400

    fixed minor spelling errors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
 //some javaScript for id connection
 
 // for the search button whnen clicked 
-$("#add-input").on("click",function(event){
+//$("#add-input").on("click",function(event){
     //prevent refresh the page
-    event.preventDefault();
+    //event.preventDefault();
     /*I made a class in html called hide. it will hide the video and ranking at
      intial with the css display none . right now just to embed this code to show 
      the data and video when user hites search.
    //$(".hide").show()*/
     //remove the  inital introduction video with id  intro-video
     //$("#intro-video").remove()
-})
+//})
 
 //variable declaration for the matching ids 
 //in line 44 this one is for capture the user input in the search box 
@@ -22,7 +22,7 @@ $("#add-input").on("click",function(event){
 //in line 71 this id is in a div for show the videos got from the youtube API in HTML
 //var characterVideo=$("#video-display");
 // the table body in start in line 100 in html and I created several rows and put some data to show the rank
-var hero = ""
+var hero = "Spider-Man"
 
 var marvelURL = "https://cors-anywhere.herokuapp.com/https://gateway.marvel.com/v1/public/characters?name=" + hero + "&ts=1&apikey=" + marvelAPIkey + "&hash=" + marvelAPIhash
 
@@ -41,7 +41,7 @@ var settings = {
     console.log(response);
     if (response.data.count === 1) {
         $("#data-display").append(response.data.results[0].name)
-        $("#data-display").append("<img src=" + response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension + " alt=hulk></img>")
+        $("#data-display").append("<img src=" + response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension + " alt=" + hero + "></img>")
         $("#data-display").append(response.data.results[0].description)
         var series = response.data.results[0].series.items
         for (var i = 0; i < series.length; i++) {
